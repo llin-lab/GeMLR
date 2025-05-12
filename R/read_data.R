406233941c7fda9faade765c5a9add0ef7b788d4
zhangjx1996 <43453519+zhangjx1996@users.noreply.github.com>
2025-05-12 08:44:52 -0400

    Update read_data.R

--- R/read_data.R
+++ R/read_data.R
@@ -76,7 +76,7 @@ read_data <- function(dat_road,sep_mark=' ', num_gmm=NULL,alphaLasso=0.8, ycol=N
   }
 
 
-  X <- cbind(X, Indi)
+  #X <- cbind(X, Indi)
   Y <- as.vector(Y)
   cv_fit <- suppressWarnings(cv.glmnet(data.matrix(X), Y, alpha = 1, family = "binomial", nfolds = 5))
   B <- coef(cv_fit, s = "lambda.min")
